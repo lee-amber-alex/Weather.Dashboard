71f14e5ece842102268f317b40ad68df6f16155c
Amber Lee <lee.amber.alex@gmail.com>
2020-10-09 22:24:34 -0700

    Added eventListener for search button and API call function to test.

---
+++ script.js
@@ -0,0 +1,44 @@
+let dateEl = $("#date");
+let cityNameEl= $("#cityName");
+let tempEl= $("#temp");
+let humidityEl= $("#humidity");
+let uvEl= $("#UV");
+let searchCityEl = $("#searchCity");
+let citySearchEl = $("#citySearch");
+let timeNow = moment().format("lll");
+
+
+dateEl.append(timeNow);
+
+function buildQueryURL(){
+let apiKeyEl = "&appid=0c4095be8ee8948edd8333313900b9cb";
+let queryParams= $("#citySearch").val().trim();
+let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + queryParams + apiKeyEl;
+return queryURL; 
+
+};
+console.log(queryURL);
+console.log(queryParams);
+
+$("#searchCity").on("click", function(event){
+event.preventDefault();
+
+let queryURL= buildQueryURL();
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    
+const results = response.data;
+console.log(response);
+
+});
+
+});
+
+
+// for (var i = 0; i < results.legnth; i++){
+// let cityName = $("<div>");
+// let p = $("<p>").text(results[i].temperature);
+// let weatherCondition = $("<img>");
